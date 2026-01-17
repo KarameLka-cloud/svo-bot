c3ffc7fa36343b96ea24767f8925c4c6406acc51
Valentin <>
2026-01-17 15:08:08 +0800

    Добавлена библиотека dotenv. Созданы команды для начала работы с ботом, включая ввод /start. Добавлено приветственное сообщение

--- svo-backend/bot.ts
+++ svo-backend/bot.ts
@@ -1,8 +1,70 @@
-import { Bot } from "@maxhub/max-bot-api";
+import dotenv from "dotenv";
+dotenv.config();
+import { Bot, Keyboard } from "@maxhub/max-bot-api";
 
-const bot: Bot = new Bot("");
+interface User {
+  id?: string;
+  name?: string;
+}
 
-bot.command("start", (ctx) => ctx.reply("Добро пожаловать!"));
+const token = process.env.BOT_TOKEN;
+if (!token) {
+  throw new Error("Environment variable BOT_TOKEN is not set");
+}
+
+const bot: Bot = new Bot(token);
+
+bot.api.setMyCommands([{ name: "start", description: "Добро пожаловать!" }]);
+
+const startKeyboard = Keyboard.inlineKeyboard([
+  [
+    Keyboard.button.link("📊 Статистика", "https://minister.service.mfc38.ru/"),
+    Keyboard.button.callback("📄 Сводка", "summary", { intent: "default" }),
+  ],
+]);
+
+function helloText(userName: string) {
+  return `<h1>👋 Здравствуйте, ${userName}.</h1>
+    <br/>
+    🤖 Я бот-помощник по вопросам военной службы по контракту в Иркутской области.
+    <br/>
+    📋 С моей помощью можно:
+      • подать заявку на службу по контракту;
+      • узнать о мерах поддержки для военнослужащих и членов их семей, ветеранов боевых действий;
+      • узнать о содействии в трудоустройстве;
+      • узнать ответы на часто задаваемые вопросы.
+    <br/>
+    📄 Список команд бота:
+    /start — Вы перезапустите бота, начнете беседу с «чистого листа».`;
+}
+
+bot.on("bot_started", async (ctx) => {
+  const user = ctx.user as unknown as User;
+  const userName = user?.name ?? "Пользователь";
+  // const userName = ctx.user.name;
+  await ctx.reply(helloText(userName), {
+    format: "html",
+  });
+
+  await ctx.reply("🗃️ Выберите интересующий раздел:", {
+    format: "html",
+    attachments: [startKeyboard],
+  });
+});
+
+bot.command("start", async (ctx) => {
+  const user = ctx.user as unknown as User;
+  const userName = user?.name ?? "Пользователь";
+  // const userName = ctx.user.name;
+  await ctx.reply(helloText(userName), {
+    format: "html",
+  });
+
+  await ctx.reply("Выберите интересующий раздел:", {
+    format: "html",
+    attachments: [startKeyboard],
+  });
+});
 
 bot.start();
 
