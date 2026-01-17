d30b57bb65395be92a0b44609cc6e2d91c827366
Valentin <>
2026-01-18 00:58:27 +0800

    Добавлена prisma orm

--- svo-backend/index.ts
+++ svo-backend/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import bot from "./bot.ts";
+import { prisma } from "./lib/prisma.ts";
+
+const userClient = prisma.user;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("Hello");
+});
+
+app.get("/users", async (_, res) => {
+  const users = await userClient.findMany();
+  res.json(users);
 });
 
 app.listen(3000, () => {
